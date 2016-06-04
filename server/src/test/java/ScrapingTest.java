8e9a1a0bf999c3b9f0039fbfe87461b3cb51dbc1
hoge <hoge@hoge>
2016-06-05 03:06:40 +0900

    no message

--- server/src/test/java/ScrapingTest.java
+++ server/src/test/java/ScrapingTest.java
@@ -11,6 +11,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +30,7 @@ import com.myapp.domain.Tyokin;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = App.class)
+@Ignore
 public class ScrapingTest {
     @Autowired
     TeamRankRepository repository;
